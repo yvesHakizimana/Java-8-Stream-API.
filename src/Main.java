abfb7acb36c012be0a7abc5b3a6dca62e1af3f17
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-06-14 10:03:24 +0200

    Finding The second largest Element using java 8 functional interfaces.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 
+import java.util.Comparator;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -7,9 +9,11 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args){
         String characters = "FollowMilindMehta";
+        List<Integer> numbers = List.of(5,8,2,13,42,56,24,36);
         var charCount = countCharacters(characters);
         System.out.println(findDuplicates(characters));
         findFirstUniqueCharacter(characters);
+        System.out.println(findTheSecondLargestElement(numbers));
     }
 
     public static Map<Character, Long> countCharacters(String str){
@@ -41,6 +45,18 @@ public class Main {
                 .orElse(null);
 
     }
+
+    public static Integer findTheSecondLargestElement(List<Integer> numbers){
+        return numbers
+                .stream().
+                sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .orElse(null);
+
+    }
+
+
 
 
 }
