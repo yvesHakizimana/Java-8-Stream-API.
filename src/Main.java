83ff51116ff5430d0bc9d2e9d5f2ea72c599f0fa
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-06-14 09:58:01 +0200

    Finding The first  Unique character.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -8,6 +9,7 @@ public class Main {
         String characters = "FollowMilindMehta";
         var charCount = countCharacters(characters);
         System.out.println(findDuplicates(characters));
+        findFirstUniqueCharacter(characters);
     }
 
     public static Map<Character, Long> countCharacters(String str){
@@ -27,5 +29,18 @@ public class Main {
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
     }
+
+    public static Character findFirstUniqueCharacter(String str){
+         return str
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() == 1).map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+
+    }
+
 
 }
