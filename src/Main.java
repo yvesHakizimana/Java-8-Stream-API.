7b542bc5c753be88df3af6f500375effe8f03cbb
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-06-14 09:51:58 +0200

    Finding Duplicate elements.

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args){
         String characters = "FollowMilindMehta";
         var charCount = countCharacters(characters);
-        System.out.println(charCount);
+        System.out.println(findDuplicates(characters));
     }
 
     public static Map<Character, Long> countCharacters(String str){
@@ -17,5 +17,15 @@ public class Main {
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 
+    public static Map<Character, Long> findDuplicates(String str){
+        return str
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() > 1)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+    }
 
 }
